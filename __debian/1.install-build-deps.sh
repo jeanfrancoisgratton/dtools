52d188a35a7475d8776d26e336b1fa7641e754f8
jeanfrancoisgratton <jean-francois@famillegratton.net>
2022-12-06 08:37:01 -0500

    Debian packaging fixes

--- __debian/1.install-build-deps.sh
+++ __debian/1.install-build-deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#echo "Installing dependencies";echo
-#sudo apt-get update && sudo apt-get install -y gcc
+echo "Installing dependencies";echo
+sudo apt-get update && sudo apt-get install -y docker-ce-cli
 echo;echo;echo "Done. Now installing the Go binaries"
 sudo /opt/bin/install_golang.sh 1.19.3 amd64
